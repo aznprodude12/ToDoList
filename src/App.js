9ad0377bb57eb27903136fa8e154c8814c07f535
aznprodude12 <hoa_ho123@yahoo.com>
2022-12-05 11:28:36 -0600

    Display To-do list
    This commit adds the following:
    - Import Bootstrap
    - Import FontAwesome Icons
    - Display To-do tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+     faCircleCheck, faPen, faTrashCan
+} from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     const [ toDo, setToDo ] = useState([
+          { id: 1, title: "Task 1", "status": true },
+          { id: 2, title: "Task 2", "status": false },
+     ]);
+
+     const [ newTask, setNewTask ] = useState('');
+     const [ updateData, setUpdateData ] = useState('');
+
+     return (
+          <div className="container App">
+              <br /><br />
+              <h2>To Do List App (ReactJS)</h2>
+              <br /><br />
+
+              { /* Display ToDos */ }
+
+              { toDo && toDo.length ? '' : 'No tasks to display' }
+
+              { toDo && toDo.map((task, index) => {
+                   return (
+                        <React.Fragment key={task.id}>
+                             <div className='col taskBg'>
+                                  <div className={task.status ? 'done' : ''}>
+                                       <span className="taskNumber">{index + 1}</span>
+                                       <span className="taskText">{task.title}</span>
+                                  </div>
+                             </div>
+                        </React.Fragment>
+                   )
+              })}
+          </div>
+     );
 }
 
 export default App;
