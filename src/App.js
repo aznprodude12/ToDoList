915f01380f314a5b961f0d63003008475dcd731f
aznprodude12 <hoa_ho123@yahoo.com>
2022-12-09 14:34:37 -0600

    Complete/Update/Delete Icons and Task Sorting
    This commit adds the Complete/Update/Delete icons for each task and also added sorting to display task by id.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
      const [ toDo, setToDo ] = useState([
-          { id: 1, title: "Task 1", "status": true },
+          { id: 1, title: "Task 1", "status": false },
           { id: 2, title: "Task 2", "status": false },
      ]);
 
@@ -26,13 +26,26 @@ function App() {
 
               { toDo && toDo.length ? '' : 'No tasks to display' }
 
-              { toDo && toDo.map((task, index) => {
+              { toDo && toDo
+                   .sort(((a, b) => a.id > b.id ? 1 : -1))
+                   .map((task, index) => {
                    return (
                         <React.Fragment key={task.id}>
                              <div className='col taskBg'>
                                   <div className={task.status ? 'done' : ''}>
                                        <span className="taskNumber">{index + 1}</span>
                                        <span className="taskText">{task.title}</span>
+                                  </div>
+                                  <div className='iconsWrap'>
+                                       <span title="Completed / Not Completed">
+                                            <FontAwesomeIcon icon={faCircleCheck} />
+                                       </span>
+                                       <span title="Edit">
+                                            <FontAwesomeIcon icon={faPen} />
+                                       </span>
+                                       <span title="Delete">
+                                            <FontAwesomeIcon icon={faTrashCan} />
+                                       </span>
                                   </div>
                              </div>
                         </React.Fragment>
